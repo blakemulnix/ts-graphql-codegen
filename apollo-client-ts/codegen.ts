ebbca131f1c8d69b40bfeebfaad633af51f41e2a
Blake Mulnix <8352409+blakemulnix@users.noreply.github.com>
2024-07-04 23:26:24 +0000

    simplify codegen config

--- apollo-client-ts/codegen.ts
+++ apollo-client-ts/codegen.ts
@@ -2,17 +2,12 @@ import { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
   schema: "../apollo-prisma-server/src/schema.graphql",
-  documents: ["./**/*.{ts,tsx}"],
+  documents: ["./**/*.ts"],
   generates: {
     "./__generated__/": {
       preset: "client",
-      plugins: [],
-      presetConfig: {
-        gqlTagName: "gql",
-      },
     },
   },
-  // ignoreNoDocuments: true
 };
 
 export default config;
